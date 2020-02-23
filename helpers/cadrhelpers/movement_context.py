21ccfd00d0927cac1a7380f5066d4d326018dca9
Markus Sommer <markus@splork.de>
2020-02-23 15:30:14 +0100

    "execute" ns movements by updating node context

--- helpers/cadrhelpers/movement_context.py
+++ helpers/cadrhelpers/movement_context.py
@@ -1,28 +1,93 @@
-from typing import List, Tuple
+import multiprocessing
+import time
+import math
+
+from cadrhelpers.dtnclient import send_context, build_url
+from typing import List, Tuple, Dict
 
 
 class NS2Movement:
+    """A node's movements as specified in a ns2 movement script
+
+    Attributes:
+        node_name: Self explanatory
+        rest_url: URL of the context-REST-interface
+        step: Current position in the list of movements
+        position: Current position in the form (x_pos, y_pos)
+        movements: List of movement commands in the form (timestamp, dest_x_pos, dest_y_pos, speed)
+    """
+
     def __init__(
-            self,
-            node_name: str,
-            start_position: Tuple[float, float],
-            movements: List[Tuple[float, float, float]]
+        self,
+        rest_url: str,
+        node_name: str,
+        start_position: Tuple[float, float],
+        movements: List[Tuple[int, float, float, float]],
     ):
-        self.start_position = start_position
-        self.movements = movements
-        self.node_name = node_name
+        self.node_name: str = node_name
+        self.rest_url: str = rest_url
+        self.position: Tuple[float, float] = start_position
+        self.movements: List[Tuple[int, float, float, float]] = movements
+        self.step: int = 0
+
+    def run(self) -> None:
+        """Performs periodic context updates when movement changes
+        Spawns a new process
+        """
+        process = multiprocessing.Process(target=self._run)
+        process.start()
+
+    def _run(self) -> None:
+        """Does the actual work"""
+        # differentiate between node who start moving immediately and those who take a while to get going
+        if self.movements[0][0] != 0:
+            time.sleep(self.movements[0][0])
+
+        vector = self.compute_vector()
+        self.update_context(vector)
+        self.move_step()
+
+        # main wait-and-update-loop
+        while self.step < len(self.movements):
+            wait_time: int = self.movements[self.step][0] - self.movements[
+                self.step - 1
+            ][0]
+            time.sleep(wait_time)
+            vector = self.compute_vector()
+            self.update_context(vector)
+            self.move_step()
+
+    def move_step(self) -> None:
+        """Update the node's internal position and step counter"""
+        self.position = (self.movements[self.step][1], self.movements[self.step][2])
+        self.step += 1
+
+    def update_context(self, vector: Tuple[float, float]) -> None:
+        """Update node context in dtnd"""
+        context: Dict[str, float] = {"x": vector[0], "y": vector[1]}
+        send_context(
+            rest_url=self.rest_url, context_name="movement", node_context=context
+        )
+
+    def compute_vector(self) -> Tuple[float, float]:
+        """Take the node's current position and destination/speed and compute the movement vector"""
+        x_move = self.movements[self.step][1] - self.position[0]
+        y_move = self.movements[self.step][2] - self.position[1]
+
+        # normalise vector
+        length: float = math.sqrt(math.pow(x_move, 2) + math.pow(y_move, 2))
+        x_move = x_move / length
+        y_move = y_move / length
+
+        # multiply with speed
+        x_move = x_move * self.movements[self.step][3]
+        y_move = y_move * self.movements[self.step][3]
+
+        return x_move, y_move
 
 
 def filter_ns2(path: str, node_name: str) -> List[str]:
-    """Reads in a ns2-movement-script and filters out all commands which are not for the specified node or commented out
-
-    Args:
-        path (str): Path to the movement-script
-        node_name (str): name of the node
-
-    Returns:
-        List[str]: Filtered list of commands
-    """
+    """Reads in a ns2-movement-script and filters out all commands which are not for the specified node or commented"""
     node_id: str = node_name[1:]
 
     commands: List[str] = []
@@ -35,10 +100,11 @@ def filter_ns2(path: str, node_name: str) -> List[str]:
     return commands
 
 
-def parse_ns2(path: str, node_name: str) -> NS2Movement:
+def generate_movement(rest_url: str, path: str, node_name: str) -> NS2Movement:
+    """Turns the ns2 text file into a NS2Movement object"""
     commands = filter_ns2(path=path, node_name=node_name)
     start_position: Tuple[float, float] = (0.0, 0.0)
-    movements: List[Tuple[float, float, float]] = []
+    movements: List[Tuple[int, float, float, float]] = []
 
     for command in commands:
         split: List[str] = command.split(" ")
@@ -47,16 +113,25 @@ def parse_ns2(path: str, node_name: str) -> NS2Movement:
         elif "Y_" in split:
             start_position = (start_position[0], float(split[3]))
         elif "setdest" in split:
+            timestamp = int(float(split[2]))
             x_dest = float(split[5])
             y_dest = float(split[6])
             speed = float(split[7][:-1])
-            movements.append((x_dest, y_dest, speed))
+            movements.append((timestamp, x_dest, y_dest, speed))
 
-    return NS2Movement(node_name=node_name, start_position=start_position, movements=movements)
+    return NS2Movement(
+        rest_url=rest_url,
+        node_name=node_name,
+        start_position=start_position,
+        movements=movements,
+    )
 
 
 if __name__ == "__main__":
-    ns2_movement = parse_ns2(
+    url = build_url(address="localhost", port=35043)
+    ns2_movement = generate_movement(
+        rest_url=url,
         path="/home/msommer/devel/cadr-evaluation/scenarios/randomWaypoint/randomWaypoint.ns_movements",
-        node_name="n2"
+        node_name="n11",
     )
+    ns2_movement._run()
