e9e6a2276b5ed335edef8aeed617aaa56f918102
Markus Sommer <markus@splork.de>
2020-03-22 22:35:32 +0100

    Compute time of sender selection

--- evaluation/log_parsers.py
+++ evaluation/log_parsers.py
@@ -125,6 +125,66 @@ def compute_bundle_runtimes(
             f.write(f"{bundle},{origin}\n")
 
 
+# gets logged when dtnd is about to forward a bundle
+start_msg = "Bundle will be forwarded"
+# these happen after routing so we will focus on these
+stop_msg_after = [
+    "Sending bundle to a CLA (ConvergenceSender)",
+    "Sending bundle failed",
+    "Sending bundle succeeded",
+    "Failed to forward bundle to any CLA"
+]
+# these occur before routing is triggered and should therefore not count
+stop_msg_before = [
+    "Bundle contains an exceeded hop count block",
+    "Bundle's primary block's lifetime is exceeded",
+    "Bundle's lifetime is expired",
+]
+
+
+def compute_sender_selection_computetimes(data_path: str, node_types: Dict[str, str], output_path: str):
+    print("Computing runtime of sender selection function")
+    with open(output_path, "w") as output:
+        output.write("routing,node,type,time\n")
+        for _, routings, _ in os.walk(data_path):
+            for routing in routings:
+                print(f"Evaluating {routing}")
+                for _, _, files in os.walk(os.path.join(data_path, routing)):
+                    for file in files:
+                        if "dtnd_run.log" in file:
+                            print(f"Parsing {file}")
+                            node_name: str = file.split("_")[0]
+                            node_type = node_types[node_name]
+                            bundles_in_processing: Dict[str, float] = {}
+                            with open(os.path.join(data_path, routing, file), "r") as f:
+                                for line in f:
+                                    if "panic" in line:
+                                        break
+
+                                    parts = line.split(" ")
+                                    bundle_id = get_bundle_id(parts)
+                                    log_time = parts[0].split('"')[1]
+                                    timestamp = log_time_to_int(log_time)
+
+                                    if start_msg in line:
+                                        bundles_in_processing[bundle_id] = timestamp
+                                        continue
+
+                                    for bad_stop in stop_msg_before:
+                                        if bad_stop in line:
+                                            if bundle_id in bundles_in_processing:
+                                                del bundles_in_processing[bundle_id]
+                                            continue
+
+                                    for good_stop in stop_msg_after:
+                                        if good_stop in line:
+                                            if bundle_id in bundles_in_processing:
+                                                processing_time = timestamp - bundles_in_processing[bundle_id]
+                                                del bundles_in_processing[bundle_id]
+                                                output.write(f"{routing},{node_name},{node_type},{processing_time}\n")
+                                            continue
+
+
 if __name__ == "__main__":
     # bundles = parse_simulation_run("/research_data/sommer2020cadr/data/prophet")
     # with open("/research_data/sommer2020cadr/occurrences_prophet.pickle", "wb") as f:
@@ -135,13 +195,20 @@ if __name__ == "__main__":
     types = node_types(
         scenario_path="/home/msommer/devel/cadr-evaluation/scenarios/wanderwege/wanderwege.xml",
     )
-    for routing in routings:
-        with open(
-            f"/research_data/sommer2020cadr/filtered_{routing}.pickle", "rb"
-        ) as f:
-            print("Loading bundles occurrences")
-            bundles = pickle.load(f)
-            compute_bundle_runtimes(routing=routing, bundles=bundles, nodes=types)
+
+    compute_sender_selection_computetimes(
+        data_path="/research_data/sommer2020cadr/data",
+        node_types=types,
+        output_path="/research_data/sommer2020cadr/selection_compute.csv"
+    )
+
+    # for routing in routings:
+    #     with open(
+    #         f"/research_data/sommer2020cadr/filtered_{routing}.pickle", "rb"
+    #     ) as f:
+    #         print("Loading bundles occurrences")
+    #         bundles = pickle.load(f)
+    #        compute_bundle_runtimes(routing=routing, bundles=bundles, nodes=types)
 
     # with open("/research_data/sommer2020cadr/bundles.csv", "w") as bundles:
     #     bundles.write("routing,bundles\n")
