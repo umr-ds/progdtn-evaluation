598563ee7c1d1f958ad563035ed05f9c9d9a0ad1
Markus Sommer <markus@splork.de>
2020-05-27 18:33:08 +0200

    Cleanup of dtnclient

--- helpers/cadrhelpers/dtnclient.py
+++ helpers/cadrhelpers/dtnclient.py
@@ -5,8 +5,15 @@ import argparse
 import base64
 import json
 from typing import Any, Dict, List
+from dataclasses import dataclass
 
 import requests
+
+
+@dataclass()
+class RESTError(Exception):
+    status_code: int
+    error: str
 
 
 def load_payload(path: str) -> str:
@@ -36,10 +43,75 @@ def build_url(address: str, port: int) -> str:
     return f"http://{address}:{port}/rest"
 
 
-def load_id(path: str) -> Dict[str, str]:
+def load_registration_data(path: str) -> Dict[str, str]:
     with open(path, "r") as f:
         ids: Dict[str, str] = json.load(f)
         return ids
+
+
+def register(
+    rest_url: str, endpoint_id: str, registration_data_file: str = ""
+) -> Dict[str, str]:
+    """Registers the client with the REST Application Agent
+
+    Args:
+        rest_url: Address + Port+ Prefix for REST actions
+        endpoint_id: BPv7 endpoint ID used for registration
+        registration_data_file: If set, registration data will be written to a file
+
+    Returns:
+        Dictionary with two fields:
+            "endpoint_id" contains the same value as was provided
+            "uuid": token received from the server which is necessary for future actions
+
+    Raises:
+        RESTError if anything goes wrong
+    """
+    id_json = json.dumps({"endpoint_id": endpoint_id})
+    response: requests.Response = requests.post(f"{rest_url}/register", data=id_json)
+    if response.status_code != 200:
+        raise RESTError(status_code=response.status_code, error=response.text)
+
+    parsed_response = response.json()
+    if parsed_response["error"]:
+        raise RESTError(
+            status_code=response.status_code, error=parsed_response["error"]
+        )
+
+    data = {"endpoint_id": endpoint_id, "uuid": parsed_response["uuid"]}
+    marshaled = json.dumps(data)
+    if registration_data_file:
+        with open(registration_data_file, "w") as f:
+            f.write(marshaled)
+    return data
+
+
+def fetch_pending(rest_url: str, uuid: str) -> List[Dict[str, Any]]:
+    """Fetch bundles addressed to this node
+
+    Args:
+        rest_url: Address + Port+ Prefix for REST actions
+        uuid: Authentication token received via the register-method.
+
+    Returns:
+        List of all pending bundle's addressed to this node as a unmarshaled JSON object
+
+    Raises:
+        RESTError if anything goes wrong
+    """
+    response: requests.Response = requests.post(
+        f"{rest_url}/fetch", data=json.dumps({"uuid": uuid})
+    )
+    if response.status_code != 200:
+        raise RESTError(status_code=response.status_code, error=response.text)
+
+    parsed_response = response.json()
+    if parsed_response["error"]:
+        raise RESTError(
+            status_code=response.status_code, error=parsed_response["error"]
+        )
+
+    return parsed_response["bundles"]
 
 
 def _submit_bundle(rest_url: str, data: Dict[str, Any]) -> None:
@@ -47,23 +119,42 @@ def _submit_bundle(rest_url: str, data: Dict[str, Any]) -> None:
         f"{rest_url}/build", data=json.dumps(data)
     )
     if response.status_code != 200:
-        print(f"Status: {response.status_code}", file=sys.stderr)
-        print(response.text, file=sys.stderr)
-    else:
-        parsed_response = response.json()
-        if parsed_response["error"]:
-            print(f"ERROR: {parsed_response['error']}", file=sys.stderr)
+        raise RESTError(status_code=response.status_code, error=response.text)
+
+    parsed_response = response.json()
+    if parsed_response["error"]:
+        raise RESTError(
+            status_code=response.status_code, error=parsed_response["error"]
+        )
 
 
 def send_bundle(
-    rest_url: str, id_file: str, destination: str, payload: str, lifetime: str = "24h"
+    rest_url: str,
+    uuid: str,
+    source: str,
+    destination: str,
+    payload: str,
+    lifetime: str = "24h",
 ) -> None:
-    ids = load_id(path=id_file)
+    """Sends a bundle via the REST application agent
+
+    Args:
+        rest_url: Address + Port+ Prefix for REST actions
+        uuid: Authentication token received via the register-method.
+        source: BPv7 endpoint ID which will be set as the bundle's source.
+                Needs to be one of the IDs of the agent's node
+        destination: BPv7 endpoint ID which will be set as the bundle's destination
+        payload: Bundle payload, should be a plaintext string or base64 encoded binary data
+        lifetime: Time until the bundle expires and is deleted from node stores
+
+    Raises:
+        RESTError if anything goes wrong
+    """
     data = {
-        "uuid": ids["uuid"],
+        "uuid": uuid,
         "arguments": {
             "destination": destination,
-            "source": ids["endpoint_id"],
+            "source": source,
             "creation_timestamp_now": 1,
             "lifetime": lifetime,
             "payload_block": payload,
@@ -74,19 +165,19 @@ def send_bundle(
 
 def send_context_bundle(
     rest_url: str,
-    id_file: str,
+    uuid: str,
+    source: str,
     destination: str,
     payload: str,
     context: Dict[str, Any],
     lifetime: str = "24h",
 ) -> None:
-    """Send a bundle with a context block"""
-    ids = load_id(path=id_file)
+    """Same as send_bundle, but adds an extension block for context data"""
     data = {
-        "uuid": ids["uuid"],
+        "uuid": uuid,
         "arguments": {
             "destination": destination,
-            "source": ids["endpoint_id"],
+            "source": source,
             "creation_timestamp_now": 1,
             "lifetime": lifetime,
             "payload_block": payload,
@@ -96,101 +187,62 @@ def send_context_bundle(
     _submit_bundle(rest_url=rest_url, data=data)
 
 
-def send_context(
-    rest_url: str, context_name: str, node_context: Dict[str, Any]
-) -> None:
+def send_context(rest_url: str, context_name: str, node_context: Dict[str, Any]) -> str:
     """Sends node context information to the routing daemon
 
     Args:
-        rest_url (str): URL of the REST-interface
+        rest_url (str): Address + Port+ Prefix for REST actions
         context_name (str): name of the context item
         node_context (Dict[str, Any]): Actual context
+
+    Raises:
+        RESTError if anything goes wrong
     """
     contest_str: str = json.dumps(node_context)
     response: requests.Response = requests.post(
         f"{rest_url}/context/{context_name}", data=contest_str
     )
     if response.status_code != 202:
-        print(f"Status: {response.status_code}", file=sys.stderr)
-        print(response.text, file=sys.stderr)
-    else:
-        print(response.text)
+        raise RESTError(status_code=response.status_code, error=response.text)
+
+    return response.text
 
 
-def get_node_context(rest_url: str) -> None:
+def get_node_context(rest_url: str) -> Dict[str, Any]:
     """Get all the Node's context information
 
     Args:
-        rest_url (str): URL of the REST-interface
+        rest_url (str): Address + Port+ Prefix for REST actions
+
+    Raises:
+        RESTError if anything goes wrong
     """
     response: requests.Response = requests.get(f"{rest_url}/context")
     if response.status_code != 200:
-        print(f"Status: {response.status_code}")
-        print(response.text, file=sys.stderr)
-    else:
-        print(response.text)
+        raise RESTError(status_code=response.status_code, error=response.text)
+
+    return response.json()
 
 
-def fetch_pending(rest_url: str, id_file: str) -> None:
-    """Fetch bundles addressed to this node"""
-    ids = load_id(path=id_file)
-    response: requests.Response = requests.post(
-        f"{rest_url}/fetch", data=json.dumps({"uuid": ids["uuid"]})
-    )
-    if response.status_code != 200:
-        print(f"Status: {response.status_code}", file=sys.stderr)
-        print(response.text, file=sys.stderr)
-    else:
-        parsed_response = response.json()
-        if parsed_response["error"]:
-            print(f"ERROR: {parsed_response['error']}", file=sys.stderr)
-        else:
-            for bundle in parsed_response["bundles"]:
-                print(bundle)
-
-
-def get_size(rest_url: str, p: bool = True) -> int:
+def get_size(rest_url: str) -> int:
     """Get size of stored bundle buffer
 
     Args:
-        rest_url: URL of the REST-interface
-        p: Whether the result should be printed to stdout
+        rest_url: Address + Port+ Prefix for REST actions
 
     Returns:
-        Size of store if request was successful, -1 otherwise
+        Size of store
+
+    Raises:
+        RESTError if anything goes wrong
     """
     response: requests.Response = requests.get(f"{rest_url}/size")
     response_text = response.text
-    if p:
-        if response.status_code != 200:
-            print(f"Status: {response.status_code}", file=sys.stderr)
-            print(response_text, file=sys.stderr)
-        else:
-            print(response_text)
 
     if response.status_code != 200:
-        return -1
-    else:
-        return int(response_text)
+        raise RESTError(status_code=response.status_code, error=response.text)
 
-
-def register(rest_url: str, endpoint_id: str, uuid_file: str) -> None:
-    id_json = json.dumps({"endpoint_id": endpoint_id})
-    response: requests.Response = requests.post(f"{rest_url}/register", data=id_json)
-    if response.status_code != 200:
-        print(f"Status: {response.status_code}", file=sys.stderr)
-        print(response.text, file=sys.stderr)
-    else:
-        parsed_response = response.json()
-        if parsed_response["error"]:
-            print(f"ERROR: {parsed_response['error']}", file=sys.stderr)
-        else:
-            print(f"UUID: {parsed_response['uuid']}")
-            data = json.dumps(
-                {"endpoint_id": endpoint_id, "uuid": parsed_response["uuid"]}
-            )
-            with open(uuid_file, "w") as f:
-                f.write(data)
+    return int(response_text)
 
 
 if __name__ == "__main__":
@@ -201,10 +253,10 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--context", help="Specify context file")
     parser.add_argument("-i", "--id", help="Node ID for registration")
     parser.add_argument(
-        "-u",
-        "--uuid",
-        default="id.json",
-        help="File containing the uuid which we get from the /register endpoint",
+        "-r",
+        "--registration_data",
+        default="registration_data.json",
+        help="File containing the registration data which we get from the /register endpoint",
     )
     parser.add_argument(
         "-cn", "--context_name", help="When sending node context, supply its name"
@@ -227,7 +279,7 @@ if __name__ == "__main__":
         help="Port of the routing REST-interface",
     )
     parser.add_argument(
-        "-r", "--recipient", help="DTN-EndpointID of the bundle's recipient"
+        "-d", "--destination", help="DTN-EndpointID of the bundle's recipient"
     )
     args = parser.parse_args()
 
@@ -237,25 +289,62 @@ if __name__ == "__main__":
         if args.action == "send":
             payload = load_payload(path=args.payload)
             if args.context:
-                context = load_context(args.context)
-                send_context_bundle(
-                    rest_url=url,
-                    id_file=args.uuid,
-                    destination=args.recipient,
-                    payload=payload,
-                    context=context,
-                )
+                registration_data = load_registration_data(path=args.registration_data)
+                try:
+                    context = load_context(args.context)
+                    send_context_bundle(
+                        rest_url=url,
+                        uuid=registration_data["uuid"],
+                        source=registration_data["endpoint_id"],
+                        destination=args.destination,
+                        payload=payload,
+                        context=context,
+                    )
+                except RESTError as err:
+                    print(f"HTTP Status: {err.status_code}", file=sys.stderr)
+                    print(f"Error: {err.error}", file=sys.stderr)
             else:
-                send_bundle(
-                    rest_url=url,
-                    id_file=args.uuid,
-                    destination=args.recipient,
-                    payload=payload,
-                )
+                try:
+                    registration_data = load_registration_data(
+                        path=args.registration_data
+                    )
+                    send_bundle(
+                        rest_url=url,
+                        uuid=registration_data["uuid"],
+                        source=registration_data["endpoint_id"],
+                        destination=args.destination,
+                        payload=payload,
+                    )
+                except RESTError as err:
+                    print(f"HTTP Status: {err.status_code}", file=sys.stderr)
+                    print(f"Error: {err.error}", file=sys.stderr)
+
         elif args.action == "fetch":
-            fetch_pending(rest_url=url, id_file=args.uuid)
+            registration_data = load_registration_data(path=args.registration_data)
+            try:
+                pending = fetch_pending(rest_url=url, uuid=registration_data["uuid"])
+                if pending:
+                    print("Pending Bundles:")
+                    for bundle in pending:
+                        print(bundle)
+                else:
+                    print("No new bundles")
+            except RESTError as err:
+                print(f"HTTP Status: {err.status_code}", file=sys.stderr)
+                print(f"Error: {err.error}", file=sys.stderr)
+
         elif args.action == "register":
-            register(rest_url=url, endpoint_id=args.id, uuid_file=args.uuid)
+            try:
+                registration_data = register(
+                    rest_url=url,
+                    endpoint_id=args.id,
+                    registration_data_file=args.registration_data,
+                )
+                print(f"Registration data: {registration_data}")
+            except RESTError as err:
+                print(f"HTTP Status: {err.status_code}", file=sys.stderr)
+                print(f"Error: {err.error}", file=sys.stderr)
+
         else:
             print("UNSUPPORTED ACTION", file=sys.stderr)
     elif args.interface == "routing":
@@ -263,13 +352,32 @@ if __name__ == "__main__":
 
         if args.action == "send":
             context = load_context(path=args.context)
-            send_context(
-                rest_url=url, context_name=args.context_name, node_context=context
-            )
-        elif args.action == "get":
-            get_node_context(rest_url=url)
+            try:
+                response = send_context(
+                    rest_url=url, context_name=args.context_name, node_context=context
+                )
+                print(response)
+            except RESTError as err:
+                print(f"HTTP Status: {err.status_code}", file=sys.stderr)
+                print(f"Error: {err.error}", file=sys.stderr)
+
+        elif args.action == "fetch":
+            try:
+                node_context = get_node_context(rest_url=url)
+                print("Node Context:")
+                print(node_context)
+            except RESTError as err:
+                print(f"HTTP Status: {err.status_code}", file=sys.stderr)
+                print(f"Error: {err.error}", file=sys.stderr)
+
         elif args.action == "size":
-            get_size(rest_url=url, p=True)
+            try:
+                size = get_size(rest_url=url)
+                print(f"Store size: {size}")
+            except RESTError as err:
+                print(f"HTTP Status: {err.status_code}", file=sys.stderr)
+                print(f"Error: {err.error}", file=sys.stderr)
+
         else:
             print("UNSUPPORTED ACTION", file=sys.stderr)
     else:
